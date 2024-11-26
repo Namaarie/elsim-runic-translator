e48ac14e9814080ba7edd2278fff28467fffe36c
Shunyu Zhao <shunyu2@ualberta.ca>
2024-11-25 23:10:49 -0700

    nicer styling

--- app/page.tsx
+++ app/page.tsx
@@ -83,7 +83,7 @@ export default function Home() {
     <div className="inline">
       {rune_images.map((image, i) => (
         <Image
-          className="inline my-10"
+          className="inline my-4"
           key={i}
           src={image}
           width={32}
@@ -96,12 +96,16 @@ export default function Home() {
   }
   
   return (
-    <div className="">
-      <form onSubmit={parse}>
-        <textarea name="sentence" className="text-neutral-950  border-neutral-400 border-2 w-full"></textarea>
-        <input type="submit"></input>
+    <div className="flex-col mt-8">
+      <form onSubmit={parse} className="mx-auto w-1/2">
+        <div className="flex-col">
+          <textarea name="sentence" placeholder="Type your English sentence here..." className="mx-auto min-h-20 w-full px-3 py-2.5 text-lg font-bold rounded-lg border focus:outline focus:outline-2 focus:outline-offset-2 bg-[#eeeeee] text-[#444444] focus:outline-[#aaaaaa] border-[#cccccc]"></textarea>
+          <div>
+            <input type="submit" value="Translate" className="block mx-auto mt-8 mb-4 font-bold rounded-lg text-lg  w-48 h-16 bg-slate-0 text-[#444444] hover:bg-slate-200 active:bg-slate-400"></input>
+          </div>
+        </div>
       </form>
-      <div>
+      <div className="mx-8">
         {populate()}
       </div>
     </div>
